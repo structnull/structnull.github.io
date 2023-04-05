f783cc244d89d6e2db44d983c6f01ed77349ed61
chiefdinkan <rahulrex123456789@gmail.com>
2023-04-05 12:24:35 +0530

    rebirth

--- js/gen.js
+++ js/gen.js
@@ -13,39 +13,70 @@ var quotes = [
 "Fear is freedom! Subjugation is liberation! Contradiction is truth! Those are the facts of this world! And you will all surrender to them, you pigs in human clothing! - Satsuki Kiryuin",
 "The paucity and preponderance of money is the potential difference that keeps the circuit of life functioning. As such, it is safe to surmise that money does give happiness. Although not always, money is akin to a pillar that affluent people can lean onto when shit goes haywire. -WishWash ball jo-knee",
 "If you are rich, you can be a bitch. -Viswas paul jhonny",
+"I am the great mighty poo and I'm going to throw my shit at you. - The Great Mighty Poo, Conker's Bad Fur Day",
+"The pleasure of a dream is that it's a fantasy. If it happens, it was never a dream to begin with! - Kuja, Final Fantasy IX",
+"The world is indeed full of peril, and in it there are many dark places; but still there is much that is fair, and though in all lands love is now mingled with grief, it grows perhaps the greater. - J.R.R. Tolkien",
+"Pain insists upon being attended to. God whispers to us in our pleasures, speaks in our conscience, but shouts in our pains: it is His megaphone to rouse a deaf world. - C.S. Lewis",
+"The wound is the place where the Light enters you. - Rumi",
+"The most authentic thing about us is our capacity to create, to overcome, to endure, to transform, to love and to be greater than our suffering. - Ben Okri",
+"He who has a why to live for can bear almost any how. - Friedrich Nietzsche",
+"It's so hard to forget pain, but it's even harder to remember sweetness. We have no scar to show for happiness. We learn so little from peace. - Chuck Palahniuk",
+"The world breaks everyone, and afterward, some are strong at the broken places. - Ernest Hemingway",
+"The only way to deal with fear is to face it head-on. - Sarah J. Maas",
+"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+"To live is to suffer, to survive is to find some meaning in the suffering. - Friedrich Nietzsche",
+"Do not let your difficulties fill you with anxiety, after all, it is only in the darkest nights that stars shine more brightly. - Hasan Al-Basri",
+"The harder the conflict, the more glorious the triumph. - Thomas Paine",
+"The only true wisdom is in knowing you know nothing. - Socrates",
+"It is only in our darkest hours that we may discover the true strength of the brilliant light within ourselves that can never, ever, be dimmed. - Doe Zantamata",
+"One thing you can't hide - is when you're crippled inside. - John Lennon",
+"The opposite of love is not hate, it's indifference. The opposite of art is not ugliness, it's indifference. The opposite of faith is not heresy, it's indifference. And the opposite of life is not death, it's indifference. - Elie Wiesel",
+"There is no greater agony than bearing an untold story inside you. - Maya Angelou",
+"We are all broken, that’s how the light gets in. - Ernest Hemingway",
+"In the depth of winter, I finally learned that within me there lay an invincible summer. - Albert Camus",
+"I have waited for this opportunity for more than half a century, to repeat to you once again my vow of eternal fidelity and everlasting love. - Gabriel Garcia Marquez",
+"We loved with a love that was more than love. - Edgar Allan Poe",
+"There are no facts, only interpretations. - Friedrich Nietzsche",
+"One must still have chaos in oneself to be able to give birth to a dancing star. - Friedrich Nietzsche",
+"“Fascism should rightly be called Corporatism, as it is the merger of corporate and government power.” - Benito Mussolini",
+"“The Western world has been thoroughly Judaized, and it’s time to do something about it.” - David Duke",
+"“Nationalism is the only way to ensure that the future generations will have the chance to live in peace and harmony, in a country that is truly their own.” - Marine Le Pen",
+"“Do not argue with an idiot. He will drag you down to his level and beat you with experience.” -Greg King",
+"“I disapprove of what you say, but I will defend to the death your right to say it.” -Voltaire",
+"“I have a dream that one day this nation will rise up and live out the true meaning of its creed: ‘We hold these truths to be self-evident, that all men are created equal.’” -Martin Luther King Jr.",
+"“The best way to predict the future is to invent it.” -Alan Kay",
+"“If you want to make enemies, try to change something.” -Woodrow Wilson",
+"“The only true wisdom is in knowing you know nothing.” -Socrates",
+"Love is an untamed force. When we try to control it, it destroys us. When we try to imprison it, it enslaves us. When we try to understand it, it leaves us feeling lost and confused. - Paulo Coelho",
+"When I let go of what I am, I become what I might be. - Lao Tzu",
 "Love is not love which alters when it alteration finds. - William Shakespeare",
 "Love is the emblem of eternity: it confounds all notion of time: effaces all memory of a beginning, all fear of an end. - Madame de Stae",
 "Love is an untamed force. When we try to control it, it destroys us. When we try to imprison it, it enslaves us. When we try to understand it, it leaves us feeling lost and confused. - Paulo Coelho",
 "Love is not a feeling of happiness. Love is a willingness to sacrifice. - Michael Novak",
+"愛についての最高のものは、それを与えることです - me",
 "When love is not madness, it is not love. - Pedro Calderón de la Barca",
 "Why is light given to those in misery, and life to the bitter of soul, to those who long for death that does not come? - Job",
 "All tremble at violence; all fear death. Putting oneself in the place of another, one should not kill nor cause another to kill. (Sutta Nipata 705)",
 "Do not repay evil with evil or insult with insult. On the contrary, repay evil with blessing, because to this you were called so that you may inherit a blessing. (1 Peter 3:9)"
 ];
-// Shuffle the quotes array
-function shuffle(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+
+// Check if all quotes have been displayed
+if (localStorage.getItem("loadedQuotes")) {
+  var loadedQuotes = JSON.parse(localStorage.getItem("loadedQuotes"));
+  if (loadedQuotes.length === quotes.length) {
+    localStorage.clear(); // Clear local storage
+    loadedQuotes = []; // Reset loadedQuotes array
   }
-  return array;
+} else {
+  var loadedQuotes = [];
 }
-shuffle(quotes);
 
 // Get a random quote that hasn't been displayed yet
-var loadedQuotes = JSON.parse(localStorage.getItem("loadedQuotes")) || [];
-var unusedQuotes = [];
-for (var i = 0; i < quotes.length; i++) {
-  if (loadedQuotes.indexOf(quotes[i]) === -1) {
-    unusedQuotes.push(quotes[i]);
-  }
-  if (unusedQuotes.length === 1) {
-    break;
-  }
-}
-var randomQuote = unusedQuotes[0];
+var unusedQuotes = quotes.filter(function(quote) {
+  return loadedQuotes.indexOf(quote) === -1;
+});
+var randomQuote = unusedQuotes[Math.floor(Math.random() * unusedQuotes.length)];
+
 // Store the current quote in local storage
 loadedQuotes.push(randomQuote);
 localStorage.setItem("loadedQuotes", JSON.stringify(loadedQuotes));
