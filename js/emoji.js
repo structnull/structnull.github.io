5703088a68f476f86d8e8a2ad265a4f2883b17b1
chiefdinkan <rahulrex123456789@gmail.com>
2024-01-14 16:25:53 +0530

    added re

---
+++ js/emoji.js
@@ -0,0 +1,137 @@
+// Define an array of emojis
+const emojis = [
+  "😈",
+  "👻",
+  "🤖",
+  "🐒",
+  "🦄",
+  "🦕",
+  "🐙",
+  "🐌",
+  "🦞",
+  "🐳",
+  "🦔",
+  "🦌",
+  "🦥",
+  "🐸",
+  "🦩",
+  "🦈",
+  "🦧",
+  "🦜",
+  "🐉",
+  "🦖",
+  "🐡",
+  "🦇",
+  "🦑",
+  "🦨",
+  "🐋",
+  "🦦",
+  "🦮",
+  "🦢",
+  "🦉",
+  "🐲",
+  "🐀",
+  "🪲",
+  "🐊",
+  "🪱",
+  "🦭",
+  "🦫",
+  "🐆",
+  "🦍",
+  "🐅",
+  "🦬",
+  "🦣",
+  "🦘",
+  "🦛",
+  "🦨",
+  "🦯",
+  "🦝",
+  "🦦",
+  "🦩",
+  "🦠",
+  "🦤",
+  "🦢",
+  "🦡",
+  "🦚",
+  "🦆",
+  "🦓",
+  "🦔",
+  "🦏",
+  "🦜",
+  "🦩",
+  "🦷",
+  "🦧",
+  "🦕",
+  "🦎",
+  "🐪",
+  "🐜",
+  "🦞",
+  "🦍",
+  "🦙",
+  "🦣",
+  "🦜",
+  "🦑",
+  "🦔",
+  "🦉",
+  "🦥",
+  "🦨",
+  "🦩",
+  "🍄",
+  "🌻",
+  "🌼",
+  "🍃",
+  "🍂",
+  "🍁",
+  "🌵",
+  "🌴",
+  "🍇",
+  "🍓",
+  "🤔",
+  "🤩",
+  "🥳",
+  "🙌",
+  "💡",
+  "👀",
+  "👍",
+  "👎",
+  "🤷",
+  "🤦"
+];
+// Check if all emojis have been displayed
+if (localStorage.getItem("loadedEmojis")) {
+  const loadedEmojis = JSON.parse(localStorage.getItem("loadedEmojis"));
+  if (loadedEmojis.length === emojis.length) {
+    localStorage.removeItem("loadedEmojis"); // Remove loadedEmojis from local storage
+  }
+} 
+
+// Function to select a random emoji from the array that hasn't been displayed yet
+function selectRandomEmoji() {
+  let loadedEmojis = JSON.parse(localStorage.getItem("loadedEmojis")) || [];
+
+  // Get an array of unused emojis
+  const unusedEmojis = emojis.filter(function(emoji) {
+    return !loadedEmojis.includes(emoji);
+  });
+
+  // If all emojis have been displayed, reset the loadedEmojis array
+  if (unusedEmojis.length === 0) {
+    loadedEmojis = [];
+  }
+
+  // Choose a random emoji from the unusedEmojis array
+  const randomEmoji = unusedEmojis[Math.floor(Math.random() * unusedEmojis.length)];
+
+  // Add the randomEmoji to the loadedEmojis array
+  loadedEmojis.push(randomEmoji);
+
+  // Store the updated loadedEmojis array in localStorage
+  localStorage.setItem("loadedEmojis", JSON.stringify(loadedEmojis));
+
+  // Return the randomEmoji
+  return randomEmoji;
+}
+
+// Select the emoji element and set its inner HTML to a random emoji
+const emojiElement = document.querySelector(".emoji");
+emojiElement.innerHTML = selectRandomEmoji();
